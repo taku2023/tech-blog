8291beca6d3e493728cb44cd92af7b3e03083797
takuaki mori <t_mori@cosmoroot.co.jp>
2023-08-15 17:02:48 +0900

    add maintenance view

---
+++ webpage/src/maintenance.tsx
@@ -0,0 +1,12 @@
+import "@/styles/all.scss"
+import ReactDOM from "react-dom/client"
+import "./maintenance.scss"
+
+ReactDOM.createRoot(document.getElementById("body") as HTMLElement).render(
+  <>
+    <div className="maintenance-layout">
+      <h1 className="headline text-center">Sorry for maintenance..</h1>
+      <h3 className="title pt-8">We're under construction</h3>
+    </div>
+  </>
+)
--- webpage/vite.config.ts
+++ webpage/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
@@ -10,5 +11,12 @@ export default defineConfig({
   build: {
     minify: false,
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        maintenance: resolve(__dirname, "maintenance.html"),
+      },
+    },
   },
+  appType: "mpa",
 })
